cd82e9ab2a0e5b864975392a6e56eb45d91acc52
Henry <quoccuongit95@gmail.com>
2023-07-13 13:51:31 +0700

    [S01-01] Initialize Project + Setup Domains.Entities (Basic)

---
+++ Cores/Domains/Entities/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace Domains.Entities
+{
+    public sealed class Company : BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Phone { get; set; }
+    }
+}
