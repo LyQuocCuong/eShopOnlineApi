d4afb6c4e6bb275385f1122b173b7ccd4109a812
Henry <quoccuongit95@gmail.com>
2023-07-27 15:50:46 +0700

    [SOA-033][Enhancement] Log the FullName of CollectionTypes (IEnumerable, List,...) in MapService.

--- Implementations/eShopOnlineUtilities/LogMessages/MapLogMessages.cs
+++ Implementations/eShopOnlineUtilities/LogMessages/MapLogMessages.cs
@@ -4,12 +4,40 @@ namespace eShopOnlineUtilities.LogMessages
 {
     internal static class MapLogMessages
     {
+        private static bool IsCollectionType(Type genericType)
+        {
+            return (typeof(System.Collections.IEnumerable).IsAssignableFrom(genericType));
+        }
+
+        private static string GetFullCollectionTypeName(Type genericType)
+        {
+            /// Example: genericType = typeof(IEnumerable<Employee>)
+
+            // genericType.Name -> "IEnumerable`1" => IEnumerable
+            string collectionType = genericType.Name.Split('`')[0];
+
+            // GetGenericArguments() -> [0]: {Name = "Employee", FullName = "Domains.Entities.Employee"}
+            string objType = genericType.GetGenericArguments()[0].Name;
+
+            return $"{collectionType}<{objType}>";
+        }
+
         public static string MappingInfo<TSource, TDestination>()
         {
             string sourceType = typeof(TSource).Name;
             string destinationType = typeof(TDestination).Name;
 
-            string content = $"Mapping from [{sourceType}] to [{destinationType}]";
+            if (IsCollectionType(typeof(TSource)))
+            {
+                sourceType = GetFullCollectionTypeName(typeof(TSource));
+            }
+
+            if (IsCollectionType(typeof(TDestination)))
+            {
+                destinationType = GetFullCollectionTypeName(typeof(TDestination));
+            }
+
+            string content = $"Mapping from {sourceType} to {destinationType}";
             return LogContentsTemplate.MapServiceFormat(content);
         }
     }
